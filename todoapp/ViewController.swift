f900c86c7ad3cd1dbade5260b8f901b3238e2878
ryo takumi <ryotakumi@ryonoMacBook-Air.local>
2020-08-13 16:54:58 +0900

    first commit

---
+++ todoapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todoapp
+//
+//  Created by ryo takumi on 2020/07/30.
+//  Copyright © 2020 ryo takumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
